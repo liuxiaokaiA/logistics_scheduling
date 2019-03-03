0bf76930ce1756ed756757e25acc989477f56d5a
System Administrator <root@appledeAir.lan>
2019-03-03 17:03:58 +0800

    fix

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 # coding: utf-8
+from log import MyLogging
+from read_configure import read_fuc
+from algorithm.ga import update_global
+from model.trunk import trunk_init
+from model.base_station import base_init
 
 
 def update(day):
@@ -13,7 +18,17 @@ def output(day):
     pass
 
 
+def init():
+    base_init()
+    trunk_init()
+
+
 if __name__ == "main":
+    MyLogging()
+    default_conf = read_fuc('conf/default.conf')
+    update_global(default_conf)
+
+    init()
     days = 100
     for day in range(days):
         update(day)
