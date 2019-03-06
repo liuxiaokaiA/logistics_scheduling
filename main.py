fe09a25e0e346a4d1d922c1f36892e38f27860cb
wangmumu <liuhaobwjc@163.com>
2019-03-06 18:53:41 +0800

    修改显示

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ from log import MyLogging
 from read_configure import read_fuc
 from algorithm.ga import update_global, GA
 from algorithm.model_data import get_trunk_max_order, get_orders_trunk_can_take, \
-    modify_model, get_whole_trunk
-from global_data import list_base, list_destination, list_trunk, all_scheduling, trunk_num, destination_num, base_num
+    modify_model, get_whole_trunk, get_orders_list
+from global_data import list_base, list_destination, list_trunk, all_scheduling, trunk_num, destination_num, \
+    base_num, gene_bits
+
+history_order_num = 0
+list_trunk_not_in_base = []
 
 
 def update(day):
@@ -34,16 +38,19 @@ def compute(day):
     get_whole_trunk()
     trunk_max_order = get_trunk_max_order()
     data = get_orders_trunk_can_take(trunk_max_order)
-    # print data
+    trunk_data, order_list = get_orders_list(trunk_max_order, data)
+    gene_len = 0
+    gene_len += len(order_list) * gene_bits
+    print ('gene length: ', len(order_list) * gene_bits, len(order_list))
     ga = GA()
     log.info('start to compute')
-    ga.GA_main(data, trunk_max_order)
+    # ga.GA_main(data, trunk_max_order)
+    ga.GA_main2(trunk_data, order_list)
     log.info('ga down.start to get best gene')
     best_gene = ga.selectBest()
-    gene_data = best_gene.gene_to_data(ga.order, ga.key_order)
+    gene_data = best_gene.gene_to_data(ga.gene_bits, ga.order_list)
     log.info('start to modify_model')
-    modify_model(gene_data)
-    all_scheduling[day] = gene_data
+    all_scheduling[day] = modify_model(gene_data, trunk_data)
 
 
 def output(day):
@@ -57,8 +64,10 @@ def output(day):
     trunk_on_road_num = 0
     trunk_in_order_destination = 0
     trunk_in_order_base = 0
-
+    temp_trunk_not_in_base = []
     for trunk in list_trunk:
+        if trunk.trunk_state == TRUNK_IN_ORDER_DESTINATION:
+            temp_trunk_not_in_base.append(trunk.trunk_id)
         if trunk.trunk_state == TRUNK_ON_ROAD or trunk.trunk_state == TRUNK_ON_ROAD_NOT_USE:
             trunk_sum += 1
             trunk_on_road_num += 1
@@ -71,6 +80,13 @@ def output(day):
             trunk_in_order_base += 1
         elif trunk.trunk_state == TRUNK_IN_ORDER_DESTINATION:
             trunk_in_order_destination += 1
+    num = 0
+    global list_trunk_not_in_base
+    for id in list_trunk_not_in_base:
+        if id not in temp_trunk_not_in_base:
+            num += 1
+    print ('异地车返回数量%d'%num)
+    list_trunk_not_in_base = temp_trunk_not_in_base
     trunk_empty_rate = (trunk_empty * 1.0) / trunk_sum
     trunk_transport_rate = (trunk_transport_car * 1.0) / trunk_sum_transport
 
@@ -103,11 +119,11 @@ def output(day):
         base.update_in_station_trunk(list_trunk)
         base_trunk_in_station_list.append(len(base.trunk_in_station))
         print("网点%d : 剩余未运订单数 :%d 本网点等计划车数目%d"
-              % (base.bid, base_sum_delay_order_list[base.bid], base_trunk_in_station_list[base.bid]))
+              % (base.b_id, base_sum_delay_order_list[base.b_id], base_trunk_in_station_list[base.b_id]))
 
     average_delay_day = (sum_delay_day * 1.0) / (order_delay_low + order_delay_middle + order_delay_high)
     print("历史订单总数为    ：%d" % history_order_num)
-    print("已经运载的订单数  ：%d" % history_order_num-sum(base_sum_delay_order_list))
+    print("已经运载的订单数  ：%d" % (history_order_num - sum(base_sum_delay_order_list)))
     print("当前空车率%f，当前搭载率%f" % (trunk_empty_rate, trunk_transport_rate))
     print("当前正在运输车辆%d，当前在base等计划车辆%d，当前异地base等计划车辆%d" % (
         trunk_on_road_num, trunk_in_order_base, trunk_in_order_destination,))
