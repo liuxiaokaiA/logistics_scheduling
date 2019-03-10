3f31bc5a7590a67f3f16c7ced7b23a0a65e51380
System Administrator <root@appledeAir.lan>
2019-03-10 15:36:30 +0800

    fix get trunk

--- model/base_station.py
+++ model/base_station.py
@@ -113,14 +113,19 @@ class BaseStation:
 
     def get_trunk(self, trunk_type=TRUNK_TYPE_SMALL):
         for id in self.trunk_in_station:
-            if trunk_type == TRUNK_TYPE_SMALL and id < trunk_num / 3:
-                return id
-            elif trunk_type == TRUNK_TYPE_MIDDLE and trunk_num * 2 / 3 > id >= trunk_num / 3:
-                return id
-            elif trunk_type == TRUNK_TYPE_BIG and id >= trunk_num * 2 / 3:
-                return id
+            if trunk_type == TRUNK_TYPE_SMALL:
+                if id < trunk_num / 3:
+                    return id
+            elif trunk_type == TRUNK_TYPE_MIDDLE:
+                if trunk_num * 2 / 3 > id >= trunk_num / 3:
+                    return id
+            elif trunk_type == TRUNK_TYPE_BIG:
+                if id >= trunk_num * 2 / 3:
+                    return id
             else:
                 return None
+        else:
+            return None
 
 
 def get_near_trunk(base, trunk_list, distance=distance_around):
